c358b4f66b524824a7d3ac2871a151e589d165cf
ravikanth.anisetty30@gmail.com <Ravikanth.Anisetty@INLPF1AX7GT.icepor.com>
2024-11-25 18:25:35 +0530

    First commit on Nov 11

---
+++ GITTest/src/main/java/Test1.java
@@ -0,0 +1,21 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	
+	int a=20;
+	int b=20;
+	
+ @Test
+public void test()
+{
+	int c=a+b;
+
+      Assert.assertEquals(true, true);
+    System.out.println("Hello");
+    System.out.println("world");
+    System.out.println("software");
+	}
+	
+}
